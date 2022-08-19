976eb2a9a13961eb18492f5495079f632355366d
Guillaume Auger <guillaume.auger@simbachain.com>
2022-08-19 14:24:47 -0400

    [BLK-644] Validate app against org

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -9,7 +9,6 @@ import {
 // import {default as prompt} from 'prompts';
 import {default as chalk} from 'chalk';
 import axios from "axios";
-import { KeycloakHandler, AzureHandler } from '@simbachain/web3-suites/dist/commands/lib/authentication';
 
 export const command = 'login';
 export const describe = 'log in to SIMBAChain SCaaS';
@@ -66,26 +65,61 @@ export const handler = async (args: yargs.Arguments): Promise<any> => {
         interactive = true;
     }
 
-    if (authStore instanceof KeycloakHandler) {
-        try {
-            await authStore.logout();
-            const org = await chooseOrganisationFromList(simbaConfig);
-            if (!org) {
-                SimbaConfig.log.error(`${chalk.redBright(`No Organisation Selected!`)}`);
-                SimbaConfig.log.debug(`:: EXIT :`);
-                return Promise.resolve(new Error('No Organisation Selected!'));
-            }
-            const app = await chooseApplicationFromList(simbaConfig);
-            if (!app) {
-                SimbaConfig.log.error(`${chalk.redBright(`simba: No Application Selected!`)}`);
-                SimbaConfig.log.debug(`:: EXIT :`);
-                return Promise.resolve(new Error('No Application Selected!'));
-            }
-            SimbaConfig.resetSimbaJson(previousSimbaJson, org);
-            SimbaConfig.log.info(`${chalk.cyanBright('\nsimba: Logged in with organisation')} ${chalk.greenBright(org.display_name)} ${chalk.cyanBright('and application')} ${chalk.greenBright(app.display_name)}`);
+    if (authStore == null) {
+        SimbaConfig.log.error(`${chalk.redBright(`\nsimba: authStore not set!`)}`);
+        return;
+    }
+
+    if (!interactive) {
+        if (org && !app) {
+            SimbaConfig.log.error(`${chalk.redBright(`\nsimba: if specifying an org in non-interactive mode, you must specify an app.`)}`);
             SimbaConfig.log.debug(`:: EXIT :`);
-            Promise.resolve(null);
-        }  catch (error) {
+            return;
+        }
+        let appData;
+        let orgData;
+        if (!org) {
+            orgData = SimbaConfig.ProjectConfigStore.get("organisation");
+            const orgName = orgData.name;
+            if (!orgName) {
+                SimbaConfig.log.error(`${chalk.redBright(`no organisation specified in your login command, and no organisation present in your simba.json. Please login in interactive mode and choose your organisation, or use the --org <org> flag in your non-interactive login command.`)}`);
+                return;
+            } else {
+                SimbaConfig.log.info(`${chalk.cyanBright(`no org was specified in login command; logging in using org ${orgName} from simba.json`)}`);
+            }
+        }
+        if (!app) {
+            appData = SimbaConfig.ProjectConfigStore.get("application");
+            const appName = appData.name;
+            if (!appName) {
+                SimbaConfig.log.error(`${chalk.redBright(`no app specified in your login command, and no application present in your simba.json. Please login in interactive mode and choose your application, or use the --app <app> flag in your non-interactive login command.`)}`);
+                return;
+            } else {
+                SimbaConfig.log.info(`${chalk.cyanBright(`no app was specified in login command; logging in using app ${appName} from simba.json`)}`);
+            }
+        }
+        authStore.logout();
+        try {
+            await authStore.performLogin(interactive);
+            if (org) {
+                orgData = await chooseOrganisationFromName(simbaConfig, org as string);
+            }
+            if (app) {
+                appData = await chooseApplicationFromName(simbaConfig, app as string);
+            }
+            if (orgData.id != appData.organisation.id) {
+                SimbaConfig.log.error(`${chalk.redBright(`the selected app is not part of the selected organisation. Please login in interactive mode and choose your application, or use the --app <app> flag in your non-interactive login command.`)}`);
+                return;
+            }
+
+            SimbaConfig.resetSimbaJson(previousSimbaJson, org);
+            SimbaConfig.organisation = orgData
+            SimbaConfig.application = appData
+                
+            SimbaConfig.log.info(`${chalk.greenBright(`Logged in to SIMBA Chain!`)}`);
+            SimbaConfig.log.debug(`:: EXIT :`);
+            return;
+        } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 SimbaConfig.log.error(`${chalk.redBright(`\nsimba: EXIT : ${JSON.stringify(error.response.data)}`)}`)
             } else {
@@ -94,80 +128,34 @@ export const handler = async (args: yargs.Arguments): Promise<any> => {
             return;
         }
     }
-
-    if (authStore instanceof AzureHandler) {
-        if (!interactive) {
-            if (org && !app) {
-                SimbaConfig.log.error(`${chalk.redBright(`\nsimba: if specifying an org in non-interactive mode, you must specify an app.`)}`);
-                SimbaConfig.log.debug(`:: EXIT :`);
-                return;
-            }
-            if (!org || !app) {
-                const orgFromSimbaJson = SimbaConfig.ProjectConfigStore.get("organisation");
-                const orgName = orgFromSimbaJson.name;
-                if (!orgName) {
-                    SimbaConfig.log.error(`${chalk.redBright(`no organisation specified in your login command, and no organisation present in your simba.json. Please login in non-inetractive mode and choose your organisation, or use the --org <org> flag in your non-interactive login command.`)}`);
-                    return;
-                } else {
-                    SimbaConfig.log.info(`${chalk.cyanBright(`no org was specified in login command; logging in using org ${orgName} from simba.json`)}`);
-                }
-                const appFromSimbaJson = SimbaConfig.ProjectConfigStore.get("application");
-                const appName = appFromSimbaJson.name;
-                if (!appName) {
-                    SimbaConfig.log.error(`${chalk.redBright(`no app specified in your login command, and no application present in your simba.json. Please login in non-inetractive mode and choose your application, or use the --app <app> flag in your non-interactive login command.`)}`);
-                    return;
-                } else {
-                    SimbaConfig.log.info(`${chalk.cyanBright(`no app was specified in login command; logging in using app ${appName} from simba.json`)}`);
-                }
-            }
-            authStore.logout();
+    try {
+        authStore.logout();
+        if (!authStore.isLoggedIn()) {
+            await authStore.performLogin();
+        } else {
             try {
-                await authStore.performLogin(interactive);
-                if (org) {
-                    await chooseOrganisationFromName(simbaConfig, org as string);
-                }
-                if (app) {
-                    await chooseApplicationFromName(simbaConfig, app as string);
-                }
-                SimbaConfig.resetSimbaJson(previousSimbaJson, org);
-                SimbaConfig.log.info(`${chalk.greenBright(`Logged in to SIMBA Chain!`)}`);
-                SimbaConfig.log.debug(`:: EXIT :`);
-                return;
-            } catch (error) {
-                if (axios.isAxiosError(error) && error.response) {
-                    SimbaConfig.log.error(`${chalk.redBright(`\nsimba: EXIT : ${JSON.stringify(error.response.data)}`)}`)
-                } else {
-                    SimbaConfig.log.error(`${chalk.redBright(`\nsimba: EXIT : ${JSON.stringify(error)}`)}`);
-                }
-                return;
-            }
-        }
-        try {
-            authStore.logout();
-            if (!authStore.isLoggedIn()) {
+                await authStore.refreshToken();
+            } catch (e) {
                 await authStore.performLogin();
-            } else {
-                try {
-                    await authStore.refreshToken();
-                } catch (e) {
-                    await authStore.performLogin();
-                }
             }
-            const org = await chooseOrganisationFromList(simbaConfig);
-            if (!org) {
-                SimbaConfig.log.debug(`:: EXIT :`);
-                return Promise.resolve(new Error('No Organisation Selected!'));
-            }
-            const app = await chooseApplicationFromList(simbaConfig);
-            if (!app) {
-                SimbaConfig.log.debug(`:: ENTER :`);
-                return Promise.resolve(new Error('No Application Selected!'));
-            }
-            SimbaConfig.resetSimbaJson(previousSimbaJson, org);
-            SimbaConfig.log.info(`${chalk.cyanBright('\nsimba: Logged in with organisation')} ${chalk.greenBright(org.display_name)} ${chalk.cyanBright('and application')} ${chalk.greenBright(app.display_name)}`);
-
-        } catch (e) {
-            SimbaConfig.log.error(`${chalk.redBright(`\nsimba: ${e}`)}`)
         }
+        const org = await chooseOrganisationFromList(simbaConfig);
+        if (!org) {
+            SimbaConfig.log.debug(`:: EXIT :`);
+            return Promise.resolve(new Error('No Organisation Selected!'));
+        }
+        const app = await chooseApplicationFromList(simbaConfig);
+        if (!app) {
+            SimbaConfig.log.debug(`:: ENTER :`);
+            return Promise.resolve(new Error('No Application Selected!'));
+        }
+        SimbaConfig.resetSimbaJson(previousSimbaJson, org);
+        SimbaConfig.organisation = org
+        SimbaConfig.application = app
+        SimbaConfig.log.info(`${chalk.cyanBright('\nsimba: Logged in with organisation')} ${chalk.greenBright(org.display_name)} ${chalk.cyanBright('and application')} ${chalk.greenBright(app.display_name)}`);
+
+    } catch (e) {
+        SimbaConfig.log.error(`${chalk.redBright(`\nsimba: ${e}`)}`)
     }
+    //}
 };
