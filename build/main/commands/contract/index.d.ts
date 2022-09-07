858c9d9ba63afddfa8a7fe961662eccf6fa35097
bpbirch <brendanpbirch@gmail.com>
2022-09-07 10:50:59 -0500

    no longer committing build files

--- build/main/commands/contract/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as PullCommand from "./pull";
-import * as ViewCommand from "./viewContracts";
-import * as AddLibCommand from "./addlibrary";
-import * as DeleteCommand from "./deletecontract";
-export declare const Pull: typeof PullCommand;
-export declare const View: typeof ViewCommand;
-export declare const AddLib: typeof AddLibCommand;
-export declare const DeleteContract: typeof DeleteCommand;
